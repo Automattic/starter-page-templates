c761cd601b1a5f430835b9ec42e2f5c62bbed1cd
Marek Hrabe <marekhrabe@me.com>
2019-05-20 21:21:06 +0200

    Limit templates for new pages only (#11)

--- wp-js-plugin-starter.php
+++ wp-js-plugin-starter.php
@@ -9,29 +9,33 @@
  * @package wp-js-plugin-starter
  */
 
- /**
+/**
  * Retrieves a URL to a file in the plugin's directory.
  *
  * @param  string $path Relative path of the desired file.
- *
  * @return string       Fully qualified URL pointing to the desired file.
- *
- * @since 1.0.0
  */
 function wp_js_plugin_starter_url( $path ) {
 	return plugins_url( $path, __FILE__ );
 }
 
 function page_templates_register() {
-    wp_register_script(
-        'plugin-sidebar-js',
-        wp_js_plugin_starter_url( 'dist/index.js' ),
-        array( 'wp-plugins', 'wp-edit-post', 'wp-element' )
-    );
+	wp_register_script(
+		'plugin-sidebar-js',
+		wp_js_plugin_starter_url( 'dist/index.js' ),
+		array( 'wp-plugins', 'wp-edit-post', 'wp-element' )
+	);
 }
 add_action( 'init', 'page_templates_register' );
 
 function page_templates_enqueue() {
-    wp_enqueue_script( 'plugin-sidebar-js' );
+	$screen = get_current_screen();
+
+	// Return early if we don't meet conditions to show templates.
+	if ( 'page' !== $screen->id || 'add' !== $screen->action ) {
+		return;
+	}
+
+	wp_enqueue_script( 'plugin-sidebar-js' );
 }
 add_action( 'enqueue_block_editor_assets', 'page_templates_enqueue' );
