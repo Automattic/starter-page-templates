33d11aac5ea86f76a9a2336308b49e15cbc86781
Dave Smith <getdavemail@gmail.com>
2019-05-22 16:54:24 +0100

    Add/template visual layout (#19)
    * Adds CSS files via SASS support
    
    * Create basic Template select component and implement styles
    
    * Adds full width modal
    
    Also brings design more into line with mockup
    
    * Renames Template Selection component
    
    * Makes image placeholder a prop rather than hardcoding
    
    * Updates Modal overlay to cover only Editor canvas
    
    * Reinstate enqueing of CSS

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import replacePlaceholders from './utils/replace-placeholders';
+import './styles/starter-page-templates-editor.scss';
+
+import TemplateSelectorControl from './components/template-selector-control';
 
 ( function( wp ) {
     const registerPlugin = wp.plugins.registerPlugin;
     const createElement = wp.element.createElement;
-    const { Modal, Button, RadioControl } = wp.components;
+    const { Modal, Button } = wp.components;
     const { withState } = wp.compose;
     
     const insertTemplate = template => {
-        console.log( 'Using template', template );
-
         // set title
         wp.data.dispatch('core/editor').editPost({title: replacePlaceholders( template.title ) } );
         
@@ -27,32 +28,53 @@ import replacePlaceholders from './utils/replace-placeholders';
         isLoading: false,
         selectedTemplate: 'home',
         templates: {
-            home: { title: 'Home', slug: 'home', contentUrl: 'https://www.mocky.io/v2/5ce525112e00006900f83afe' },
-            menu: { title: 'Menu', slug: 'menu', contentUrl: 'https://www.mocky.io/v2/5ce525112e00006900f83afe' },
-            contact: { title: 'Contact Us', slug: 'contact', contentUrl: 'https://www.mocky.io/v2/5ce525112e00006900f83afe' },
+            home: { title: 'Home', slug: 'home', contentUrl: 'https://www.mocky.io/v2/5ce525112e00006900f83afe', imgSrc: 'https://via.placeholder.com/200x180', },
+            menu: { title: 'Menu', slug: 'menu', contentUrl: 'https://www.mocky.io/v2/5ce525112e00006900f83afe', imgSrc: 'https://via.placeholder.com/200x180', },
+            contact: { title: 'Contact Us', slug: 'contact', contentUrl: 'https://www.mocky.io/v2/5ce525112e00006900f83afe', imgSrc: 'https://via.placeholder.com/200x180', },
         },
     } )( ( { isOpen, selectedTemplate, templates, setState } ) => (
         <div>
             { isOpen && (
                 <Modal
                     title="Select Page Template"
-                    onRequestClose={ () => setState( { isOpen: false } ) }>
-                    <RadioControl
-                        label="Template"
-                        selected={ selectedTemplate }
-                        options={ Object.values( templates ).map( template => ( { label: template.title, value: template.slug } ) ) }
-                        onChange={ ( selectedTemplate ) => { setState( { selectedTemplate } ) } }
-                    />
-                    <div>
-                        <Button isDefault isLarge onClick={ () => setState( { isOpen: false } ) }>
-                            Start with blank page
-                        </Button>
-                        <Button isPrimary isLarge onClick={ () => {
-                            setState( { isOpen: false } );
-                            insertTemplate( templates[ selectedTemplate ] );
-                        } }>
-                            Use Template
-                        </Button>
+                    onRequestClose={ () => setState( { isOpen: false } ) }
+                    className="st-template-selector__modal"
+                    >
+                    <div className="st-template-selector">
+                        <div className="st-template-selector__inner">
+
+                            <div className="st-template-selector__intro">
+                                <p>Pick a Template that matches the purpose of your page.</p>
+                                <p>You can customise each Template to meet your needs.</p>
+                            </div>
+                            <form className="st-template-form">
+                                <fieldset className="st-template-list">
+                                    <TemplateSelectorControl
+                                        label="Template"
+                                        selected={ selectedTemplate }
+                                        templates={ Object.values( templates ).map( template => ( {
+                                             label: template.title, 
+                                             value: template.slug,
+                                             preview: template.imgSrc
+                                         } ) ) }
+                                        onChange={ ( selectedTemplate ) => { setState( { selectedTemplate } ) } }
+                                    />
+                                </fieldset>
+                                <div class="st-template-actions">
+                                    <Button className="st-template-action st-template-action--use" isPrimary isLarge onClick={ () => {
+                                        setState( { isOpen: false } );
+                                        insertTemplate( templates[ selectedTemplate ] );
+                                    } }>
+                                        Use Template
+                                    </Button>
+                                    or 
+                                    <Button className="st-template-action" isLink isLarge onClick={ () => setState( { isOpen: false } ) }>
+                                        Start with blank page
+                                    </Button>
+                                </div>
+                            </form>
+
+                        </div>
                     </div>
                 </Modal>
             ) }
